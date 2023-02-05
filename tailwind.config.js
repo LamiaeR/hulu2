2e7a2e5691785d9a4d483cfc6ed66bac3b972998
LamiaeR <lamiaerichard@gmail.com>
2023-02-05 17:38:09 +0100

    screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        "3xl":"2000px",
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
